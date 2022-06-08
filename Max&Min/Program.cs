8e5b717ed451c69bc4071522b57d14fb7050b4c5
Ekaterina <pechnikova.07@mail.ru>
2022-06-08 19:43:39 +0300

    Max and Mim of two numbers

---
+++ Max&Min/Program.cs
@@ -0,0 +1,25 @@
+﻿// **Задача 2:** Напишите программу, которая на вход принимает 
+//два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 ->  max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
+if(A > B)
+{
+  max = A;
+  min = B;
+}
+else
+{
+    max = B;
+    min = A;
+}
+Console.WriteLine($"Максимальное число {max}");
+Console.WriteLine($"Минимальное число {min}");
